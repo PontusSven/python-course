859118023699281534634d392931fc584c35f784
Pontussven <pontus.svensson92@gmail.com>
2020-06-09 15:55:57 +0200

    formatting

--- strings.py
+++ strings.py
@@ -21,6 +21,22 @@ print(name.upper())
 x = "Hello Word"
 print(x.split("l"))
 
+# .format() 3 ways to do
+print("Hello my name is {} and I am {} years old".format("Pontus", 27))
+print("The {0} {2} {1}".format("fox", "fast", "is"))
+print("The {f} {d} {b}".format(f="fox", b="fast", d="is"))
+
+#float formatting
+result = 100 / 777
+print("the result was {r:1.5f}".format(r = result))
+
+# f string literal
+name = "Jose"
+age = 34
+print(f"His name is {name} and he is {age} years old")
+
+
+
 
 
 
