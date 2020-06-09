54dd9d02b73ad0fed3cc741b43c73addd6a008c9
Pontussven <pontus.svensson92@gmail.com>
2020-06-09 11:14:33 +0200

    numbers update

---
+++ numbers.py
@@ -0,0 +1,12 @@
+# print(7 % 4)
+# odd / even number checker
+# divide by 2
+# if return is 0 = even
+# if return is 1 = uneven
+number = 23 % 2
+
+if number == 0: 
+    print('number is even')
+elif number == 1:
+    print('number is uneven')
+
