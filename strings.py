ab8f7d4025256424d14dbf934066e0f9203a64c9
Pontussven <pontus.svensson92@gmail.com>
2020-06-09 15:26:37 +0200

    string extended

--- strings.py
+++ strings.py
@@ -1,4 +1,30 @@
 name = "Pontus"
 print(name[2])
 slice_name = slice(name[0], name[3])
-print(slice_name)
+
+mystring = "abcdefghijk"
+print(mystring)
+print(mystring[:3])
+print(mystring[3:6])
+# every second character
+print(mystring[::2])
+# reverse string by index
+print(mystring[::-1])
+
+# Immutability
+letter = "z"
+print(letter * 10)
+name = "Sam"
+print(name.upper())
+
+#split() no parameter splits by whitespace
+x = "Hello Word"
+print(x.split("l"))
+
+
+
+
+
+
+
+
