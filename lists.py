69324376ffc98503060faaa4436775ccebadce2b
Pontussven <pontus.svensson92@gmail.com>
2020-06-11 08:45:10 +0200

    lists

---
+++ lists.py
@@ -0,0 +1,17 @@
+my_list = [1,2,3,4,5]
+mixed_list = ["hello", 1, "World", 4]
+
+len(my_list)
+
+# concat list
+total_list = my_list + mixed_list
+
+letter_list = ['a', 'c', 'y', 'r', 'b']
+letter_list.append("g")
+print(letter_list)
+
+letter_list.pop()
+print(letter_list)
+
+letter_list.sort()
+print(letter_list)
