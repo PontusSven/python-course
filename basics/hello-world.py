85de259f8f9ca7172e87ae584488e1a1e4f43de4
Pontussven <pontus.svensson92@gmail.com>
2020-06-12 12:36:31 +0200

    statements and operators

--- basics/hello-world.py
+++ basics/hello-world.py
@@ -1,1 +1,8 @@
 print('hello world')
+
+list3 = [1,2,[3,4,'hello']]
+
+list3[2][2] = 'goodbye'
+
+print(list3)
+
---
+++ comparison-operators/operators.py
@@ -0,0 +1,18 @@
+# True
+2 == 2
+'hello' == 'hello'
+3 != 3
+2 >= 2
+
+
+# False
+2 == 1
+2 == '2'
+'hello' == 'bye'
+3 < 5
+
+# Mutliple comparisons
+
+2 < 3 < 4
+1 < 2 and 2 < 3
+2 == 2 or 1 == 1 
