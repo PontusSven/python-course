fc9dc62159afb5808825a64471870740ed898b85
Pontussven <pontus.svensson92@gmail.com>
2020-06-15 16:41:44 +0200

    started on func

--- comparison-operators/operators.py
+++ comparison-operators/operators.py
@@ -16,3 +16,17 @@
 2 < 3 < 4
 1 < 2 and 2 < 3
 2 == 2 or 1 == 1 
+
+
+
+""" myString = "Word"
+myList = []
+
+for letter in myString:
+    myList.append(letter)
+
+print(myList) """
+
+myString = 'Wordtwo'
+myList = [letter for letter in myString]
+print(myList)
